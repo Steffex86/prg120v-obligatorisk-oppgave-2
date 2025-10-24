d3c1aef2b6745387c5aebda373acc9f173b03840
Steffex86 <steff_valseth@hotmail.com>
2025-10-24 22:31:46 +0200

    Remove cellpadding attribute from table

--- vis-alle-klasser.php
+++ vis-alle-klasser.php
@@ -12,7 +12,7 @@
   $antallRader=mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */
 
   print ("<h3>Klasser</h3>");
-  print ("<table border=1 cellpadding="5">");  
+  print ("<table border=1>");  
   print ("<tr> <th align=left>Klassekode</th> <th align=left>Klassenavn</th> <th align=left>Studiumkode</th> </tr>"); 
 
   for ($r=1;$r<=$antallRader;$r++)
